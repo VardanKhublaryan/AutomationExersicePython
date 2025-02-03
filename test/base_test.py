e787cb46189001ce204a9289ab7f0903c6cdd0ad
user <vardankhublaryan2@gmail.com>
2025-02-03 22:47:48 +0400

    add requirement.txt file

--- test/base_test.py
+++ test/base_test.py
@@ -1,18 +1,21 @@
-import unittest
+import pytest
 from src.utils.web_driver_singleton import WebDriverSingleton
 from src.utils.wait_helper import WaitHelper
 
 
-class BaseTest(unittest.TestCase):
+@pytest.mark.usefixtures("setup_teardown")  # Automatically applies to all derived test classes
+class BaseTest:
+    driver = None;
+    """Base test class for driver setup and teardown"""
 
-    def setUp(self):
-        """Runs once before all test methods in a test class."""
+    @pytest.fixture(scope="function", autouse=True)
+    def setup_teardown(self, request):
+        """Fixture to initialize and quit the driver before and after each test."""
         self.driver = WebDriverSingleton.get_driver()
         wait = WaitHelper(self.driver)
         wait.wait_for_page_loaded()
         self.driver.get("https://automationexercise.com/")
 
+        yield  # This makes sure tests run
 
-    def tearDown(self):
-        """Runs once after all test methods in a test class."""
         WebDriverSingleton.quit_driver()
